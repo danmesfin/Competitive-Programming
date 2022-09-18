8fe19c8be15a7ae698e62b20b2aa3edb16bc23f7
Daniel <79213142+danmesfin@users.noreply.github.com>
2022-09-19 00:13:06 +0300

    Create InsertionSort.py

---
+++ InsertionSort.py
@@ -0,0 +1,14 @@
+def insertionSort1(n, arr):
+    # Write your code here
+    j = n-1
+    store = arr[j]
+    
+    for i in range(j, -1, -1):
+        if store < arr[i-1] and i >= 1:
+            arr[i] = arr[i-1]
+            print(' '.join(str(x) for x in arr))
+        else: 
+            arr[i] = store
+            print(' '.join(str(x) for x in arr))
+            break 
+    
