4b76b1e5014d4c9dab508776880f5b461b581719
Daniel Mesfin <79213142+danmesfin@users.noreply.github.com>
2023-03-25 21:53:48 +0300

    updated - using monotonic stack

--- Next Greater Element.py
+++ Next Greater Element.py
@@ -1,24 +1,40 @@
-#brute force solution
+# #brute force solution
+
+# class Solution:
+#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         result = []
+#         for i in range(len(nums1)):
+#             idx = 0
+#             temp = 0
+#             added = False
+#             for j in range(len(nums2)):
+#                 if nums1[i] == nums2[j]:
+#                     idx = nums2.index(nums2[j])
+#                     temp = nums2[j]
+#                     for k in range(idx, len(nums2)):
+#                         if nums2[k] > temp:
+#                             result.append(nums2[k])
+#                             added = True
+#                             break
+#                     if not added:
+#                         result.append(-1)
+#         return result
+######################################### Using monotonic Stack #################################################
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        result = []
-        for i in range(len(nums1)):
-            idx = 0
-            temp = 0
-            added = False
-            for j in range(len(nums2)):
-                if nums1[i] == nums2[j]:
-                    idx = nums2.index(nums2[j])
-                    temp = nums2[j]
-                    for k in range(idx, len(nums2)):
-                        if nums2[k] > temp:
-                            result.append(nums2[k])
-                            added = True
-                            break
-                    if not added:
-                        result.append(-1)
-        return result
+        stack = []
+        next_greater = {}
+        for num in nums2:
+            while stack and num > stack[-1]:
+                next_greater[stack.pop()] = num
+            stack.append(num)
+        while stack:
+            next_greater[stack.pop()] = -1
+        
+        return [next_greater[num] for num in nums1]
 
+
+                
 
                 
